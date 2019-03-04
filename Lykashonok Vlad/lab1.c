9c2e430e4076b572511955d1fb1394be47393d7f
Lykashonok <48084905+Lykashonok@users.noreply.github.com>
2019-03-04 12:33:55 +0300

    Create lab1.c

---
+++ Lykashonok Vlad/lab1.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+//14 вариант
+
+_Bool isPerfect(int);
+
+int main()
+{
+	long int max;
+	printf_s("Input border\n");
+	scanf_s("%i", &max);
+	for (int number = 1; number < max; number++)
+	{
+		//просматривает каждое число на идеальность
+		if (isPerfect(number))
+		{
+			printf_s("%d\n", number);
+		}
+	}
+	printf_s("done");
+	scanf_s("%i", &max);
+	return 0;
+}
+
+_Bool isPerfect(int number)
+{
+	int divider_sum = 0;
+	for (int divider = 1; divider < number - 1; divider++)
+	{
+		// сумма делителей
+		if (!(number%divider))
+			divider_sum += divider;
+	}
+	//если сумма делителей равна числу, то вернёт 1
+	return number == divider_sum;
+}
+
+
+//6,
+//28,
+//496,
+//8128,
+//33 550 336, Уже на этом числе время проверки > 10 минут (действительно в результате находит)
+//8 589 869 056
