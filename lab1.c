6e42750a890d3b7d7ef366a912c5cb2a96120c38
JanushFM <48549833+JanushFM@users.noreply.github.com>
2019-03-15 10:53:12 +0300

    Update2

--- lab1.c
+++ lab1.c
@@ -9,9 +9,13 @@ int main()
 {
 	setlocale(LC_ALL, "RUS");
 	int number;
-	printf("Введите натуральное число: ");
-	scanf_s("%d", &number);
-	printf("\n");
+	for (int i = 0; true; i++)
+	{
+		printf("Введите натуральное число: ");
+		scanf_s("%d", &number);
+		if (number > 0) break;
+		else printf("Ваше число не натуральное, попробуйте ещё раз\n");
+	}
 	char str[100];
 	_itoa_s(number, str, 10);					
 	int size = 0;
@@ -22,20 +26,20 @@ int main()
 	bool incr = true, red = true,wrong=false;
 	for (int i = 0; str[i]!='\0'; i++)
 	{
-		if (str[i + 1] > str[i] && str[i + 1] != '\0')
+		if (str[i + 1] > str[i] && str[i + 1] != '\0') //проверка на возрастание
 		{
 			red = false;
 		}
-		else if (str[i + 1] < str[i] && str[i + 1] != '\0')
+		else if (str[i + 1] < str[i] && str[i + 1] != '\0') //проверка на убывание
 		{
 			incr = false;
 		}
-		else if (str[i + 1] == str[i])
+		else if (str[i + 1] == str[i])//в случае если идут подряд одинаковые числа
 		{
 			wrong = true;
 			break;
 		}
-		else if (red == false && incr == false)
+		else if (red == false && incr == false)//если только часть числа упорядочена 
 		{
 			wrong = true;
 			break;
