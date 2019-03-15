8985360d5ebed506dd5b52d4ba09b62e26d4412f
DaniilTS <48275635+DaniilTS@users.noreply.github.com>
2019-03-16 01:06:29 +0300

    The_Final_Variant

--- laba1.c
+++ laba1.c
@@ -1,10 +1,6 @@
 //Вариант 30
-#pragma hdrstop
-#pragma argsused
-
 #include <stdio.h>
-#include <locale.h>
-
+#include <conio.h>
 #ifdef _WIN32
 #include <tchar.h>
 #else
@@ -12,27 +8,40 @@
   #define _tmain main
 #endif
 
+void search(int k)
+{
+	int a;
+	if (k%2==0)//нахождение цифр, которые являются единицами
+	{
+		a=k/2;
+		a=a+10;
+		a=a%10;//нахожу остаток от деления, тоесть цифры, которые являются единицами
+		printf("%d",a-1);//вывод цифр являющихся единицами
+	}
+	else//нахождение цифр, которые являются десятками
+	{
+		k=k+1;//делаю нецелое число целым
+		a=k/2;
+		a=a+10;
+		a=a/10;//нахожу частное, тоесть цифры, которые являются десятками
+		printf("%d",a);//Вывод чисел являющихся десятками
+	}
+}
+
 int _tmain(int argc, _TCHAR* argv[])
 {
-	int i,k;
-	start:system("cls");
-	printf("Vvedite k-toe chislo posledovatelnosty, kotoroe vi hotite vivesti.\n");
-	scanf("%d",&k);
-	if (k<0||k>89)
+	int k;
+	while (1)
 	{
-		goto start;
-	}
-
-	system("cls");
-
-	for (i = 0; i < 90; i++)
-	{
-		if (i==k)
+		printf("Vvedite k-uy cifru, chto vi hotite naity:");
+		scanf("%d",&k);
+        system("cls");
+		if (k>=1&&k<=196)
 		{
-			printf("%d ",i+10);
+			search(k);//функция поиска и вывода К-той цифры из последовательности
+			break;
 		}
 	}
-
-	system ("pause");
+	getch();
 	return 0;
 }
