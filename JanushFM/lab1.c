602c3e0a6f2288c24e9f18f386414c902011ad66
JanushFM <48549833+JanushFM@users.noreply.github.com>
2019-03-16 13:43:14 +0300

    Update lab1.c

--- JanushFM/lab1.c
+++ JanushFM/lab1.c
@@ -7,43 +7,37 @@ int main()
 {
 	setlocale(LC_ALL, "RUS");
 	int number;
-	for (int i = 0; true; i++)
+	do
 	{
 		printf("Введите натуральное число: ");
 		scanf_s("%d", &number);
-		if (number > 0) break;
-		else printf("Ваше число не натуральное, попробуйте ещё раз\n");
-	}
+		if (number<=0) printf("Ваше число не натуральное, попробуйте ещё раз\n");
+	} while (number <= 0);
 	char str[100];
 	_itoa_s(number, str, 10);
-	int size = 0;
-	for (int i = 0; str[i] != '\0'; i++)
-	{
-		size++;
-	}
 	bool incr = true, red = true, wrong = false;
-	for (int i = 0; str[i] != '\0'; i++)
+	for (int i = 0; str[i+1] != '\0'; i++)
 	{
-		if (str[i + 1] > str[i] && str[i + 1] != '\0') //проверка на возрастание
+		if (str[i + 1] > str[i]) //проверка на возрастание
 		{
 			red = false;
 		}
-		else if (str[i + 1] < str[i] && str[i + 1] != '\0') //проверка на убывание
+		else if (str[i + 1] < str[i]) //проверка на убывание
 		{
 			incr = false;
 		}
-		else if (str[i + 1] == str[i])//в случае если идут подряд одинаковые числа
+		if (str[i + 1] == str[i])//в случае если идут подряд одинаковые числа
 		{
 			wrong = true;
 			break;
 		}
-		else if (red == false && incr == false)//если только часть числа упорядочена 
+		if (red == false && incr == false)//если только часть числа упорядочена 
 		{
 			wrong = true;
 			break;
 		}
 	}
-	if (wrong == true || size < 2) printf("Ваша число не является упорядоченными по возврастанию, или убыванию");
+	if (wrong == true || str[1] =='\0') printf("Ваша число не является упорядоченными по возврастанию, или убыванию");
 	else
 	{
 		if (incr) printf("Цифры упорядочены по возрастанию");
