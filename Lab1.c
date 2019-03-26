b3e9d8c94398627474eaa992cf59a9d786b57cda
Hoce-one-love <47945033+Hoce-one-love@users.noreply.github.com>
2019-03-26 08:36:47 +0300

    Update Lab1.c

--- Lab1.c
+++ Lab1.c
@@ -5,30 +5,30 @@ int check(int);
 
 int main()
 {
-	int start_value = 0, value_three = 1, value_four = 0, buffer;//buffer -- временная переменная
+	int start_value = 0, value, buffer;//buffer -- временная переменная
 	printf("Input number from 100 to 9999: ");
 	start_value = check(start_value);//Проверяем введённое с клавиатуры числo
 	if (start_value < 1000)//случай, если число трёхзначное
 	{
+		value = 1;
 		while (start_value > 0)//условие выхода
 		{
 			buffer = start_value % 10;
 			start_value /= 10;
-			value_three *= buffer;
+			value *= buffer;
 		}
-		printf("value_three= %d", value_three);
 	} 
-	else //случай, если число четрыхзначное
+	else //случай, если число четырёхзначное
 	{
+		value = 0;
 		while (start_value > 0)//условие выхода
 		{
 			buffer = start_value % 10;
 			start_value /= 10;
-			value_four += buffer;
+			value += buffer;
 		}
-		printf("value_four= %d", value_four);
 	}
-	
+	printf("value= %d", value);
 	return 0;
 }
 
@@ -41,3 +41,4 @@ int check(int n)
 		rewind(stdin);
 	}
 	return n;
+}
