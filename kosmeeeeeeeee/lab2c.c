2e9171fb758a1ad2554f6918a048023c78a5d7df
Kosmeee <44471504+Kosmeee@users.noreply.github.com>
2019-05-18 14:35:27 +0300

    Add files via upload

---
+++ kosmeeeeeeeee/lab2c.c
@@ -0,0 +1,59 @@
+﻿#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+	int weight=0, dist=0,trucks;
+	while (true)
+	{
+		printf("1 - Make order\n2 - Show info about my order\n3 - About\n4 - Exit\n");
+		int c = _getch()-48;
+		if (c > 0 && c < 5)
+		{
+			switch (c)
+			{
+			case 1:
+				
+				while (true)
+				{
+					printf("input weight of load in t: ");
+					scanf_s("%d", &weight);
+					if (weight < 20 || weight > 300)
+					{
+						printf("wrong input\n");
+						continue;
+					}
+					printf("input distance in km: ");
+					scanf_s("%d", &dist);
+					if (dist > 4000)
+					{
+						printf("wrong input\n");
+						continue;
+					}
+					printf("ur order succesfully accepted\n");
+					break;
+					
+				}
+				break;
+			case 2:
+				if (!weight && !dist)
+				{
+					printf("u have no order\n");
+					
+				}
+				else
+				{
+					trucks = (weight + 19) / 20;
+					printf("needed amount of trucks: %d\n", trucks);
+					printf("price for ur order with insurance: %d$\n", int(trucks*dist * 2 * 1.05));
+					printf("price for insurance %d$\n", int(trucks*dist * 2 * 0.05));
+					
+				}
+				break;
+			case 3:printf("Version: 1.0.0\n Author: Artur Matsuk\n");
+				break;
+			case 4: return 0;
+			}
+		}
+   }
+}
---
+++ kosmeeeeeeeee/task3.c
@@ -0,0 +1,70 @@
+﻿#include <stdio.h>
+#include <string.h>
+#include <cmath>
+int numbers(int k)
+{
+	int res = 0;
+	while (k)
+	{
+		k = k / 10;
+		res++;
+	}
+	return res;
+}
+
+int main()
+{
+	bool flag = 1;
+	int cur=0,k=1;
+	char str[1000];
+	gets_s(str);
+	for (int i = 0; i < strlen(str); i++)
+	{
+		if (str[i] == str[i + 1] &&str[i] == str[i + 2])
+		{
+			for (int j = 1; flag; j++)
+			{
+				if (str[i] == str[i + j])
+					k++;
+				else
+				{
+					flag = 0;
+					if (k > 9)
+					{
+						int temp =numbers(k)-1;
+						int n=k;
+						char ch = str[i + 1];
+						while (temp>=0)
+						{
+							
+							
+							str[cur] = n / pow(10, temp)+'0';
+								n = n % int(pow(10,temp));
+								cur++;
+								temp--;
+						}
+						str[cur] = ch;
+					}
+					else
+					{
+						str[cur] = k + '0';
+						str[cur + 1] = str[i + 1];
+						cur++;
+					}
+					i += k - 1;
+					
+					k = 1;
+				}
+			}
+		flag = 1;
+		}
+		str[cur] = str[i];
+		cur++;
+	}
+	str[cur] = '\0';
+	for (int i = 0; i < strlen(str); i++)
+	{
+		printf_s("%c", str[i]);
+	}
+}
+
