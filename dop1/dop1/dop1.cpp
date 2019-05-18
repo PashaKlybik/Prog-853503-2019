2c058e47371b8484cc93e4e1be584f11ef3edcda
SacovichVl <vladkvadrat3000@gmail.com>
2019-05-18 14:33:51 +0400

    labs

---
+++ dop1/dop1/dop1.cpp
@@ -0,0 +1,46 @@
+﻿#include "pch.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+bool checkComposition(long long int a)
+{
+	bool f = false, f1 = false;
+	long long i = 0, b = a;
+	long long int *arr;
+	while (b)
+	{
+		b /= 10;
+		i++;
+	}
+	arr = (long long int*)malloc(i * sizeof(long long int));
+	for (long long j = 0; j < i; j++)
+	{
+		arr[j] = a % 10;
+		a /= 10;
+	}
+	for (int j = 0; j < i; j++)
+	{
+		if (arr[j] == 4) f = true;
+		if (arr[j] == 7) f1 = true;
+	}
+	if (f == true && f1 == true) { return true; }
+	else { return false; }
+}
+
+int main()
+{
+	long long b,n=0;
+	scanf_s("%lld", &b);
+	for (long long i = 1; i < b; i++)
+	{
+		if (b % i == 0)
+		{
+			if (checkComposition(i) == true)
+			{
+				printf("%lld\n", i);
+				n++;
+			}
+		}
+	}
+	printf("number of almost happy divisors  %lld\n", n);
+}
---
+++ dop2/dop2.cpp
@@ -0,0 +1,50 @@
+﻿
+#include <stdio.h>
+#include <string.h>
+#include <malloc.h>
+#include <ctype.h>
+
+void mainFunction(int *arr,int amount) 
+{
+	int min, repeat;
+	scanf_s("\n%d", &repeat);
+	while (repeat)
+	{
+	min = arr[0];
+		for (int i = 0; i < amount; i++)
+		{
+			if (arr[i] < min) min = arr[i];
+		}
+		for (int i = 0; i < amount; i++)
+		{
+			arr[i] = min - arr[i];
+		}
+		for (int i = 0; i < amount; i++)
+		{
+			printf("%d ", arr[i]);
+		}
+		printf("\n");
+		repeat--;
+	}
+}
+
+int main()
+{
+	int amount, * arr, min, repeat=0,sum=0;
+	scanf_s("%d", &amount);
+	arr = (int*)malloc(amount * sizeof(int));
+	for (int i = 0; i < amount; i++)
+	{
+		scanf_s("%d", &arr[i]);
+	}
+	for (int i = 0; i < amount; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	mainFunction(arr,amount);
+	for (int i = 0; i < amount; i++) 
+	{
+		sum += arr[i];
+	}
+	printf("%d", sum);
+}
