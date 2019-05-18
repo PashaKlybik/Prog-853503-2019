495cd69f13991b41b14aa79b2c6e36538dd74739
KseniyaTonko <48217336+KseniyaTonko@users.noreply.github.com>
2019-05-18 14:17:39 +0300

    Update Z1.c

--- KseniyaTonko/Z1.c
+++ KseniyaTonko/Z1.c
@@ -2,29 +2,30 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <math.h>
-
+#include <locale.h>
 int main()
 {
+	setlocale(LC_ALL, "Rus");
 	long N, answer = 0;
 	printf_s("%s", "Enter N: ");
 	scanf_s("%li", &N);
 	for (long i = 1; i <= N; i++)			
-		if (N % i == 0)											//ïðîâåðêà, ÿâëÿåòñÿ ëè ÷èñëî äåëèòåëåì N
+		if (N % i == 0)											//Ã¯Ã°Ã®Ã¢Ã¥Ã°ÃªÃ , Ã¿Ã¢Ã«Ã¿Ã¥Ã²Ã±Ã¿ Ã«Ã¨ Ã·Ã¨Ã±Ã«Ã® Ã¤Ã¥Ã«Ã¨Ã²Ã¥Ã«Ã¥Ã¬ N
 		{
 			long number = i;
 			bool isHappy = false;
 			while (number > 0)
 			{
-				if ((number % 10 == 4) || (number % 10 == 7))	//ïîèñê öèôð, êîòîðûå äîëæíû áûòü â ïî÷òè ñ÷àñòëèâîì ÷èñëå 
+				if ((number % 10 == 4) || (number % 10 == 7))	//Ã¯Ã®Ã¨Ã±Ãª Ã¶Ã¨Ã´Ã°, ÃªÃ®Ã²Ã®Ã°Ã»Ã¥ Ã¤Ã®Ã«Ã¦Ã­Ã» Ã¡Ã»Ã²Ã¼ Ã¢ Ã¯Ã®Ã·Ã²Ã¨ Ã±Ã·Ã Ã±Ã²Ã«Ã¨Ã¢Ã®Ã¬ Ã·Ã¨Ã±Ã«Ã¥ 
 					isHappy = true;
 				number = (long)(number / 10);
 			}
 			if (isHappy)
-				answer++;										//åñëè ÷èñëî ïî÷òè ñ÷àñòëèå, óâåëè÷èâàåì îòâåò íà 1
+				answer++;										//Ã¥Ã±Ã«Ã¨ Ã·Ã¨Ã±Ã«Ã® Ã¯Ã®Ã·Ã²Ã¨ Ã±Ã·Ã Ã±Ã²Ã«Ã¨Ã¥, Ã³Ã¢Ã¥Ã«Ã¨Ã·Ã¨Ã¢Ã Ã¥Ã¬ Ã®Ã²Ã¢Ã¥Ã² Ã­Ã  1
 		}
 	long number = N;
 	bool isHappy = false;
-	while (number > 0)											//ïðîâåðêà, ÿâëÿåòñÿ ëè ÷èñëî N ïî÷òè ñ÷àñòëèâûì
+	while (number > 0)											//Ã¯Ã°Ã®Ã¢Ã¥Ã°ÃªÃ , Ã¿Ã¢Ã«Ã¿Ã¥Ã²Ã±Ã¿ Ã«Ã¨ Ã·Ã¨Ã±Ã«Ã® N Ã¯Ã®Ã·Ã²Ã¨ Ã±Ã·Ã Ã±Ã²Ã«Ã¨Ã¢Ã»Ã¬
 	{
 		if ((number % 10 == 4) || (number % 10 == 7))
 			isHappy = true;
